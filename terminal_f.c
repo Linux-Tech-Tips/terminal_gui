3c5b9c864883a371822e34368f748b3127d7ec41
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2024-02-15 14:59:55 +0000

    Updated comments in terminal_f files

--- terminal_f.c
+++ terminal_f.c
@@ -1,91 +1,87 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h> // UNIX std library - not multiplatform
-#include <sys/ioctl.h>
-#include <poll.h>
-#include <stdbool.h>
-
 #include "terminal_f.h"
 
-// VARIABLES
+/* GLOBAL VARIABLES */
 
 static struct termios oldTerm;
 
-// TERMINAL FUNCTIONS
 
-/* Resets all modes (styles and colors) */
+/* TERMINAL FUNCTION IMPLEMENTATIONS */
+
 void modeReset() {
 	printf(ESCAPE "0m");
 }
 
-/* Sets the mode */
 void modeSet(int style, int fgColor, int bgColor) {
 	printf(ESCAPE);
+	/* Printing style code if desired */
 	if(style != NO_CODE) printf("%i", style);
 	printf(";");
+	/* Printing foreground color code if desired */
 	if(fgColor != NO_CODE) printf("%i", fgColor);
+	/* Printing background color code if desired */
 	if(bgColor != NO_CODE) printf(";%i", bgColor);
 	printf("m");
 }
 
-/* Sets a 256 color mode */
 void mode256Color(int bg, int colorCode) {
+	/* Deciding whether to print a background or foreground 256-color code */
 	int mode = bg ? 48 : 38;
 	printf(ESCAPE "%i;5;%im", mode, colorCode);
 }
 
-/* Erases the screen */
 void erase() {
 	printf(ESCAPE "2J");
 }
 
-/* Erases on the current line, according to the specified variable */
 void eraseLine(int lineType) {
 	printf(ESCAPE "%iK", lineType);
 }
 
-/* Move cursor to home position (0,0) */
 void cursorHome() {
 	printf(ESCAPE "H");
 }
 
-/* Move cursor to specified column and line (x, y) */
 void cursorMoveTo(int column, int line) {
 	printf(ESCAPE "%i;%iH", line, column);
 }
 
-/* Move cursor by the specified amount in the set dir */
 void cursorMoveBy(char dir, int amount) {
 	printf(ESCAPE "%i%c", amount, dir);
 }
 
-/* Gets terminal dimensions from the kernel, works even if terminal window is resized */
 void getTerminalSize(int * x, int * y) {
 	struct winsize size;
+	/* Getting current terminal size from Kernel call */
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
 	*x = size.ws_col;
 	*y = size.ws_row;
 }
 
-/* Sets terminal options for unbuffered read */
 void startKeys() {
 	struct termios newTerm;
+	/* Saving current terminal attributes into the oldTerm global structure */
 	tcgetattr(STDIN_FILENO, &oldTerm);
 	newTerm = oldTerm;
+	/* Setting the correct terminal attributes for non-blocking input */
 	newTerm.c_lflag &= ~(ICANON | ECHO);
 	tcsetattr(STDIN_FILENO, TCSANOW, &newTerm);
 }
 
-/* Non-blocking character read, needs startKeys to be called first, returns whether any characters read */
-bool nbRead(char * buffer, size_t maxToRead) {
-	bool result = 0;
+void endKeys() {
+	/* Resetting terminal attributes to the ones saved before non-blocking input started */
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldTerm);
+}
+
+short nbRead(char * buffer, size_t maxToRead) {
+	short result = 0;
 	struct pollfd fds;
 	fds.fd = 1;
-	fds.events = POLLIN;
+	fds.events = POLLIN; /* POLLIN - the type of events the program is looking for - user input */
+	/* Using the poll() system function to see if any characters ready to be read in STDIN */
 	int ready = poll(&fds, 1, 0);
 	char c;
 	int r = 0;
+	/* Reading from stdin into buffer until no longer ready or EOF reached */
 	while(ready > 0 && read(STDIN_FILENO, &c, 1) > 0) {
 		result = 1;
 		if(r < maxToRead)
@@ -94,11 +90,6 @@ bool nbRead(char * buffer, size_t maxToRead) {
 		++r;
 	}
 	return result;
-}
-
-/* Resets terminal options */
-void endKeys() {
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldTerm);
 }
 
 void cursorHide() {
--- terminal_f.h
+++ terminal_f.h
@@ -1,11 +1,28 @@
+/** 
+ * @file terminal_f.h 
+ * @brief header file declaring custom functions for terminal GUI using ANSI Sequences
+*/
 #ifndef ESC_FUNC_H
 #define ESC_FUNC_H
 
-/* Escape code defines */
+
+/* Includes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h> /* UNIX std library - not multiplatform */
+#include <sys/ioctl.h>
+#include <poll.h>
+
+
+/* === ESCAPE CODE CONSTANT DEFINITIONS === */
+
 #define ESCAPE "\x1b["
 #define ESCAPE_PURE "\x1b"
 #define NO_CODE -1
-// Foreground colors
+
+/* Foreground colors */
 #define FG_BLACK 30
 #define FG_RED 31
 #define FG_GREEN 32
@@ -15,7 +32,8 @@
 #define FG_CYAN 36
 #define FG_WHITE 37
 #define FG_DEFAULT 39
-// Background colors
+
+/* Background colors */
 #define BG_BLACK 40
 #define BG_RED 41
 #define BG_GREEN 42
@@ -25,7 +43,8 @@
 #define BG_CYAN 46
 #define BG_WHITE 47
 #define BG_DEFAULT 49
-// Styles
+
+/* Styles */
 #define STYLE_BOLD 1
 #define STYLE_FAINT 2
 #define STYLE_ITALIC 3
@@ -34,58 +53,74 @@
 #define STYLE_REVERSE 7
 #define STYLE_HIDDEN 8
 #define STYLE_STRIKETHROUGH 9
-// Directions
+
+/* Directions */
 #define UP 'A'
 #define DOWN 'B'
 #define LEFT 'D'
 #define RIGHT 'C'
-// Line erase options
+
+/* Line erase options */
 #define LINE_TO_END 0
 #define LINE_FROM_START 1
 #define LINE_ALL 2
 
-// TERMINAL FUNCTIONS
 
-/* Resets all modes (styles and colors) */
+/* === TERMINAL FUNCTIONS === */
+
+/** Resets all modes (styles and colors) */
 void modeReset();
 
-/* Sets the mode */
+/** Sets the mode of the cursor to the selected style, foreground and background colors */
 void modeSet(int style, int fgColor, int bgColor);
 
-/* Sets a 256 color mode */
+/** Sets the mode of the cursor to 256-color, using the provided color code 
+ *  @param bg boolean value deciding whether the color is set to background (1) or foreground (0)
+*/
 void mode256Color(int bg, int colorCode);
 
-/* Erases the screen */
+/** Erases the screen */
 void erase();
 
-/* Erases on the current line, according to the specified variable */
+/** Erases characters on the current line based on the provided line type 
+ *  @param lineType recommended to use the provided constants: LINE_TO_END | LINE_FROM_START | LINE_ALL
+*/
 void eraseLine(int lineType);
 
-/* Move cursor to home position (0,0) */
+/** Move the cursor to the home position (0,0) */
 void cursorHome();
 
-/* Move cursor to specified column and line (x, y) */
+/** Move the cursor to the specified column and line (x, y) */
 void cursorMoveTo(int column, int line);
 
-/* Move cursor by the specified amount in the set dir */
+/** Move the cursor by the specified amount in the set direction
+ *  @param dir for the direction, recommended to use the provided constants: UP | DOWN | LEFT | RIGHT
+ */
 void cursorMoveBy(char dir, int amount);
 
-/* Gets terminal dimensions from the kernel, works even if terminal window is resized */
+/** Saves the current terminal dimensions into the parameters x and y */
 void getTerminalSize(int * x, int * y);
 
-/* Sets terminal options for unbuffered read */
+/** Sets the required terminal options for non-blocking read */
 void startKeys();
 
-/* Non-blocking character read, needs startKeys to be called first */
-bool nbRead(char * buffer, size_t maxToRead);
-
-/* Resets terminal options */
+/** Resets the terminal options from non-blocking read back to normal */
 void endKeys();
 
+/** Non-blocking character read, reads keyboard input asynchronously while the program is running in the terminal 
+ * Before using this function, set the required settings using the startKeys() function. Afterwards, reset the options using endKeys()
+ * @returns 1 if any characters read successfully, otherwise 0
+*/
+short nbRead(char * buffer, size_t maxToRead);
+
+/** Sets the cursor to be invisible */
 void cursorHide();
+/** Sets the cursor to be visible */
 void cursorShow();
 
+/** Saves the current state and options of the terminal */
 void screenSave();
+/** Resets the current state and options of the terminal */
 void screenRestore();
 
 #endif
